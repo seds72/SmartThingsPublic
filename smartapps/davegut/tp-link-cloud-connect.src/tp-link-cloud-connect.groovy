aeb9416e729e747c4d6cb3fde26e2d7bbc5a96a6
Scott Strittmatter <scott.strittmatter@gmail.com>
2018-12-01 15:00:35 -0600

    update

--- smartapps/davegut/tp-link-cloud-connect.src/tp-link-cloud-connect.groovy
+++ smartapps/davegut/tp-link-cloud-connect.src/tp-link-cloud-connect.groovy
@@ -177,10 +177,13 @@ def addDevices() {
 	def tpLinkModel = [:]
 	//	Plug-Switch Devices (no energy monitor capability)
 	tpLinkModel << ["HS100" : "(Cloud) TP-Link Plug-Switch"]			//	HS100
+	tpLinkModel << ["HS103" : "(Cloud) TP-Link Plug-Switch"]			//	HS103
 	tpLinkModel << ["HS105" : "(Cloud) TP-Link Plug-Switch"]			//	HS105
 	tpLinkModel << ["HS200" : "(Cloud) TP-Link Plug-Switch"]			//	HS200
 	tpLinkModel << ["HS210" : "(Cloud) TP-Link Plug-Switch"]			//	HS210
 	tpLinkModel << ["KP100" : "(Cloud) TP-Link Plug-Switch"]			//	KP100
+	//	Dimming Plug Devices
+	tpLinkModel << ["HS220" : "(Cloud) TP-Link Dimming Switch"]			//	HS220
 	//	Energy Monitor Plugs
 	tpLinkModel << ["HS110" : "(Cloud) TP-Link EnergyMonitor Plug"]		//	HS110
 	tpLinkModel << ["HS115" : "(Cloud) TP-Link EnergyMonitor Plug"]		//	HS110
@@ -188,12 +191,15 @@ def addDevices() {
 	tpLinkModel << ["KB100" : "(Cloud) TP-Link SoftWhite Bulb"]			//	KB100
 	tpLinkModel << ["LB100" : "(Cloud) TP-Link SoftWhite Bulb"]			//	LB100
 	tpLinkModel << ["LB110" : "(Cloud) TP-Link SoftWhite Bulb"]			//	LB110
+	tpLinkModel << ["KL110" : "(Cloud) TP-Link SoftWhite Bulb"]			//	KL110
 	tpLinkModel << ["LB200" : "(Cloud) TP-Link SoftWhite Bulb"]			//	LB200
 	//	Tunable White Bulbs
 	tpLinkModel << ["LB120" : "(Cloud) TP-Link TunableWhite Bulb"]		//	LB120
+	tpLinkModel << ["KL120" : "(Cloud) TP-Link TunableWhite Bulb"]		//	KL120
 	//	Color Bulbs
 	tpLinkModel << ["KB130" : "(Cloud) TP-Link Color Bulb"]				//	KB130
 	tpLinkModel << ["LB130" : "(Cloud) TP-Link Color Bulb"]				//	LB130
+	tpLinkModel << ["KL130" : "(Cloud) TP-Link Color Bulb"]				//	KL130
 	tpLinkModel << ["LB230" : "(Cloud) TP-Link Color Bulb"]				//	LB230
 
 	def hub = location.hubs[0]
